ea6ad4805666ef198d48ba4e5b530fafa39cb575
Oliver <olnor18@student.sdu.dk>
2020-11-11 19:26:33 +0100

    Added getUnsavedViews test for exitaction

--- JHotDraw/src/test/java/org/jhotdraw/app/action/ExitActionTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/app/action/ExitActionTest.java
@@ -7,6 +7,12 @@ import org.junit.Test;
 
 import javax.swing.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 
@@ -45,5 +51,35 @@ public class ExitActionTest extends AbstractSaveBeforeActionTest {
         doCallRealMethod().when(mockAction).actionPerformed(any());
         mockAction.actionPerformed(null);
         verify(mockAction).SaveAndCloseIfUnsaved();
+    }
+
+    @Test
+    public void testGetUnsavedViewsTrue(){
+        ExitAction mockAction = mock(ExitAction.class);
+        mockAction.app = mockApplication;
+        doCallRealMethod().when(mockAction).getApplication();
+        doCallRealMethod().when(mockAction).getUnsavedViews();
+        View v = mock(View.class);
+        doReturn(true).when(v).hasUnsavedChanges();
+        List<View> views = new ArrayList<>();
+        views.add(v);
+        doReturn(views).when(mockApplication).views();
+        List<View> testResult = mockAction.getUnsavedViews();
+        assertFalse(testResult.isEmpty());
+    }
+
+    @Test
+    public void testGetUnsavedViewsFalse(){
+        ExitAction mockAction = mock(ExitAction.class);
+        mockAction.app = mockApplication;
+        doCallRealMethod().when(mockAction).getApplication();
+        doCallRealMethod().when(mockAction).getUnsavedViews();
+        View v = mock(View.class);
+        doReturn(false).when(v).hasUnsavedChanges();
+        List<View> views = new ArrayList<>();
+        views.add(v);
+        doReturn(views).when(mockApplication).views();
+        List<View> testResult = mockAction.getUnsavedViews();
+        assertTrue(testResult.isEmpty());
     }
 }
