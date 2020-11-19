d9889b09f7f8455ffe69764d5e94481821fbeae4
Symaro <37690011+Symaro@users.noreply.github.com>
2020-11-19 12:43:49 +0100

    initial unit test

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGEllipseFigureTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.figures;
+
+import org.jhotdraw.samples.svg.figures.SVGEllipseFigure;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author John
+ */
+public class SVGEllipseFigureTest
+{
+
+
+    public SVGEllipseFigureTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void clone_createIdenticalClone()
+    {
+        SVGEllipseFigure ellipseFigure = new SVGEllipseFigure(2, 3, 4, 5);
+        SVGEllipseFigure ellipseClone;
+
+        ellipseClone = ellipseFigure.clone();
+
+        //Asserts that the dimensions of the two objects are the same
+        assertTrue(ellipseClone.getHeight() == ellipseFigure.getHeight());
+        assertTrue(ellipseClone.getWidth() == ellipseFigure.getWidth());
+        //Asserts that the objects are not the same
+        assertTrue(ellipseClone != ellipseFigure);
+    }
+
+    @Test
+    public void
+}
